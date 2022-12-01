8ac113c8064692000e346559c6705bde8258420c
SokiSotos <sokisotos@hotmail.com>
2022-12-01 05:58:14 +0100

    Ej17

---
+++ Ejercicio17.java
@@ -0,0 +1,15 @@
+import librerias.Varias;
+public class Ejercicio17 {
+    public static void main(String[] args) {
+        long decimal = 0;     
+        System.out.print("Introduzca un número binario: ");
+        long binario = Long.parseLong(System.console().readLine());    
+        int bits = Varias.digitos(binario);    
+         
+        for(int i = 0; i < bits; i++) {
+        decimal += Varias.digitoN(binario, bits - i - 1) * Varias.potencia(2, i);
+        }
+        
+        System.out.println(binario + " en binario es " + decimal + " en decimal.");
+    }
+}
