dfa53a57342f5bde2761ca8da1454f19e9263d35
SokiSotos <sokisotos@hotmail.com>
2022-12-01 05:58:38 +0100

    Ej15

--- Ejercicio15.java
+++ Ejercicio15.java
@@ -1,7 +1,6 @@
 import librerias.Varias;
 public class Ejercicio15 {
   public static void main(String[] args) {
-
     for(int i = 1; i < 1001; i++) {
       if (Varias.esPrimo(i)) {
         System.out.print(i + "  ");
